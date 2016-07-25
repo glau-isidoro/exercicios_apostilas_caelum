31ff9cff4aa6519b56a984cee11703176a5dc07e
glau-isidoro <glaucia.isidoro@gmail.com>
2016-07-25 20:07:18 -0300

    refazendo a apostila do fj21 - até exercicio 2.11

---
+++ exercicios-apostilas-caelum/java-desenvolvimento-web/fj21-jdbc/src/br/com/caelum/jdbc/teste/TestaInsere.java
@@ -0,0 +1,24 @@
+package br.com.caelum.jdbc.teste;
+
+import java.util.Calendar;
+
+import br.com.caelum.jdbc.dao.ContatoDAO;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaInsere {
+
+	public static void main(String[] args) {
+
+		Contato contato = new Contato();
+		contato.setNome("Gláucia");
+		contato.setEmail("glaucia@email.com");
+		contato.setEndereço("Rua Minha'Rua, 666");
+		contato.setDataNascimento(Calendar.getInstance());
+		
+		ContatoDAO dao = new ContatoDAO();
+		dao.adiciona(contato);
+		
+		System.out.println("Gravado!");
+	}
+
+}
